af8e4ea0043dd5061eec4e37235abc4d9a5ed91d
John Armentor <johnparmentor@gmail.com>
2024-04-25 19:41:19 -0500

    https adjustments

--- FunnyLittleWebsite/Program.cs
+++ FunnyLittleWebsite/Program.cs
@@ -6,17 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Configure the application to listen on HTTPS port specified by an environment variable
+var port = Environment.GetEnvironmentVariable("PORT") ?? "443"; // Default to 443 if PORT env variable is not set
+builder.WebHost.UseUrls($"https://*:{port}");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts();  // Enforce HTTPS by using HSTS (ensure your deployment environment supports HTTPS)
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection(); // Redirect HTTP to HTTPS
 
 app.UseStaticFiles();
 app.UseAntiforgery();
